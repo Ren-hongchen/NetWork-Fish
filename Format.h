426e99f9429df829438d83666db1ed751ef6b2cf
Ren-hongchen <1280656287@qq.com>
2021-11-17 22:12:56 +0800

    section 5

--- Format.h
+++ Format.h
@@ -25,4 +25,43 @@ typedef struct ip_header {
     u_int des_addr;
 } IP_HEADER;
 
+typedef struct tcp_header {
+    u_short src_port;
+    u_short des_port;
+    u_int sequence_number;
+    u_int ack_number;
+    u_char header_length;
+    u_char flags;
+    u_short window_size;
+    u_short checksum;
+    u_short urgent_pointer;
+} TCP_HEADER;
+
+typedef struct udp_header{
+    u_short src_port;
+    u_short des_port;
+    u_short data_length;
+    u_short checksum;
+} UDP_HEADER;
+
+typedef struct arp_header{
+    u_short type;
+    u_short protocol;
+    u_char mac_len;
+    u_char ip_len;
+    u_short op_type;
+    u_char src_eth_addr[6];
+    u_char src_ip_addr[4];
+    u_char des_eth_addr[6];
+    u_char des_ip_addr[4];
+} ARP_HEADER;
+
+typedef struct icmp_header{
+    u_char type;
+    u_char code;
+    u_short checksum;
+    u_short identification;
+    u_short sequence;
+} ICMP_HEADER;
+
 #endif // FORMAT_H
